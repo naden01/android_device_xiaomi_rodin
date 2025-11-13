e31d2b67573b67b4cd7ba55211b456c7a6666a2c
Zeyy <zeydann.sc@gmail.com>
2025-11-13 20:40:19 +0700

    rodin: bring up InfinityX
    Signed-off-by: Zeyy <zeydann.sc@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -185,7 +185,7 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
      $(DEVICE_PATH)/framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml \
+    vendor/infinity/config/device_framework_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml
 
