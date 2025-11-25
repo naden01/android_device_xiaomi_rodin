e9708247eed93283696da4f4387b1d8d8fb37d44
nazephyrus <126683488+naden01@users.noreply.github.com>
2025-11-25 06:03:39 -0800

    rodin : lineage

--- lineage_rodin.mk
+++ lineage_rodin.mk
@@ -8,30 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common afterlife stuff.
-$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
-
-# AfterLife flags
-AFTERLIFE_MAINTAINER := Nazephyrus
-
-# Bootanimation
-TARGET_BOOT_ANIMATION_RES := 1080
-
-# FOD
-TARGET_UDFPS_ANIMATIONS := true
-
-# Offline Charging
-USE_PIXEL_CHARGING := true
-
-# disable/enable blur support, default is false
-TARGET_SUPPORTS_BLUR := true
-
-# Face Unlock
-TARGET_FACE_UNLOCK_SUPPORTED := true
-
-# Gapps
-AFTERLIFE_GAPPS := true
-GAPPS_CORE := true
+# Inherit some common lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from rodin device
 $(call inherit-product, device/xiaomi/rodin/device.mk)
